8be759b4100d609d4ff7e8afbf983718554f48f6
James Park <james.hansung.park@gmail.com>
2021-12-31 13:14:05 -0500

    redirects to product page after product creation

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e77f88719c38a3b2df61"; }
+/******/ 		__webpack_require__.h = function() { return "883cd70c7719b0b7b599"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
