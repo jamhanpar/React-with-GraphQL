f2c47a4061f26c602e3ce92158a26f5580f26913
James Park <james.hansung.park@gmail.com>
2022-03-13 18:55:31 -0400

    config settings updated

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "04c118c36772c615600f"; }
+/******/ 		__webpack_require__.h = function() { return "534b82c24180ea6c837b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
